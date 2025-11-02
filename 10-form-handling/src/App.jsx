0fee6629490cd9a1ff10242d078764ff62991c6d
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2025-11-02 21:55:32 +0600

    Form handling updated

---
+++ 10-form-handling/src/App.jsx
@@ -0,0 +1,20 @@
+const App = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+      >
+        <input type="text" placeholder="Enter your Name" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;

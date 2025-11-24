e18433e9233aca0836b532efa389b56c8601e77a
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2025-11-25 00:45:43 +0600

    React router dom basics done

--- 17-react-router-dom/src/App.jsx
+++ 17-react-router-dom/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </div>
   );

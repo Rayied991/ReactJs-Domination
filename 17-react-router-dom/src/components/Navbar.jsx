311437f2261c93639d3b5a91428fcb96d639bb07
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2025-11-25 00:32:35 +0600

    React router dom started

---
+++ 17-react-router-dom/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <h3>Navbar</h3>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/contact"}>Contact</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
